620514da50ab48b12e178c47fd86ea92d7203871
fonnymunkey <fonnymunkey@gmail.com>
2023-09-16 12:29:00 -0400

    Bump internal version

--- src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
+++ src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
@@ -52,7 +52,7 @@ import java.util.Random;
 public class IceAndFire {
 
     public static final String MODID = "iceandfire";
-    public static final String VERSION = "1.7.1";
+    public static final String VERSION = "2.0.0";
     public static final String LLIBRARY_VERSION = "1.7.9";
     public static final String NAME = "Ice And Fire";
     public static final Logger logger = LogManager.getLogger(NAME);
