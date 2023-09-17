41a5f156f3256f7dc5c62a29393e7cb6977c9443
kotlin-programmer <seattlesoftwareengineer@gmail.com>
2023-09-16 17:45:55 -0700

    Update ModelSilverArmor

--- src/main/java/com/github/alexthe666/iceandfire/client/model/ModelSilverArmor.java
+++ src/main/java/com/github/alexthe666/iceandfire/client/model/ModelSilverArmor.java
@@ -19,36 +19,37 @@ public class ModelSilverArmor extends ModelBiped {
         this.textureHeight = 64;
         this.faceGuard = new ModelRenderer(this, 30, 47);
         this.faceGuard.setRotationPoint(0F, -6.6F, 1.9F);
-        this.faceGuard.addBox(-4.5F, -3.0F, -6.1F, 9, 9, 8);
-        this.setRotateAngle(faceGuard, -0.7285004297824331F, 0.0F, 0.0F);
+        this.faceGuard.addBox(-4.5F, -3.0F, -6.1F, 9, 9, 8, modelSize);
+        this.setRotateAngle(faceGuard, -0.7285004297824331F, 0F, 0F);
+        this.robeLowerBack = new ModelRenderer(this, 4, 55);
+        this.robeLowerBack.mirror = true;
+        this.robeLowerBack.setRotationPoint(0F, 12.0F, 0F);
+        this.robeLowerBack.addBox(-4.0F, 0F, -2.5F, 8, 8, 1, modelSize);
+        this.setRotateAngle(robeLowerBack, 0F, 3.141592653589793F, 0F);
+        this.crest = new ModelRenderer(this, 18, 32);
+        this.crest.setRotationPoint(0F, -7.9F, -0.1F);
+        this.crest.addBox(0F, -0.5F, 0F, 1, 9, 9, 0F);
+        this.setRotateAngle(crest, 1.2292353921796064F, 0F, 0F);
+        this.robeLower = new ModelRenderer(this, 4, 55);
+        this.robeLower.setRotationPoint(0F, 12.0F, 0F);
+        this.robeLower.addBox(-4.0F, 0F, -2.5F, 8, 8, 1, modelSize);
         this.helmWingR = new ModelRenderer(this, 2, 37);
         this.helmWingR.setRotationPoint(-3.0F, -6.3F, 1.3F);
-        this.helmWingR.addBox(-0.5F, -1.0F, 0.0F, 1, 4, 6);
+        this.helmWingR.addBox(-0.5F, -1.0F, 0F, 1, 4, 6, modelSize);
         this.setRotateAngle(helmWingR, 0.5235987755982988F, -0.4363323129985824F, -0.05235987755982988F);
         this.helmWingL = new ModelRenderer(this, 2, 37);
         this.helmWingL.mirror = true;
         this.helmWingL.setRotationPoint(3.0F, -6.3F, 1.3F);
-        this.helmWingL.addBox(-0.5F, -1.0F, 0.0F, 1, 4, 6);
+        this.helmWingL.addBox(-0.5F, -1.0F, 0F, 1, 4, 6, modelSize);
         this.setRotateAngle(helmWingL, 0.5235987755982988F, 0.4363323129985824F, 0.05235987755982988F);
-        this.crest = new ModelRenderer(this, 18, 32);
-        this.crest.setRotationPoint(0.0F, -7.9F, -0.1F);
-        this.crest.addBox(0.0F, -0.5F, 0.0F, 1, 9, 9);
-        this.setRotateAngle(crest, 1.2292353921796064F, 0.0F, 0.0F);
-        this.robeLowerBack = new ModelRenderer(this, 4, 55);
-        this.robeLowerBack.mirror = true;
-        this.robeLowerBack.setRotationPoint(0.0F, 12.0F, 0.0F);
-        this.robeLowerBack.addBox(-4.0F, 0.0F, -2.5F, 8, 8, 1);
-        this.setRotateAngle(robeLowerBack, 0.0F, 3.141592653589793F, 0.0F);
-        this.robeLower = new ModelRenderer(this, 4, 55);
-        this.robeLower.setRotationPoint(0.0F, 12.0F, 0.0F);
-        this.robeLower.addBox(-4.0F, 0.0F, -2.5F, 8, 8, 1);
-        this.bipedHead.addChild(this.faceGuard);
-        this.bipedHead.addChild(this.helmWingR);
-        this.bipedHead.addChild(this.helmWingL);
         this.bipedHeadwear.addChild(this.crest);
+        this.bipedHead.addChild(this.faceGuard);
         this.bipedBody.addChild(this.robeLowerBack);
         this.bipedBody.addChild(this.robeLower);
+        this.bipedHead.addChild(this.helmWingR);
+        this.bipedHead.addChild(this.helmWingL);
     }
+
 
     public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
         modelRenderer.rotateAngleX = x;
@@ -58,11 +59,11 @@ public class ModelSilverArmor extends ModelBiped {
 
     public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn) {
         if (entityIn instanceof EntityArmorStand) {
-            EntityArmorStand entityarmorstand = (EntityArmorStand)entityIn;
+            EntityArmorStand entityarmorstand = (EntityArmorStand) entityIn;
             this.bipedHead.rotateAngleX = 0.017453292F * entityarmorstand.getHeadRotation().getX();
             this.bipedHead.rotateAngleY = 0.017453292F * entityarmorstand.getHeadRotation().getY();
             this.bipedHead.rotateAngleZ = 0.017453292F * entityarmorstand.getHeadRotation().getZ();
-            this.bipedHead.setRotationPoint(0.0F, 1.0F, 0.0F);
+            this.bipedHead.setRotationPoint(0F, 1.0F, 0F);
             this.bipedBody.rotateAngleX = 0.017453292F * entityarmorstand.getBodyRotation().getX();
             this.bipedBody.rotateAngleY = 0.017453292F * entityarmorstand.getBodyRotation().getY();
             this.bipedBody.rotateAngleZ = 0.017453292F * entityarmorstand.getBodyRotation().getZ();
@@ -75,14 +76,25 @@ public class ModelSilverArmor extends ModelBiped {
             this.bipedLeftLeg.rotateAngleX = 0.017453292F * entityarmorstand.getLeftLegRotation().getX();
             this.bipedLeftLeg.rotateAngleY = 0.017453292F * entityarmorstand.getLeftLegRotation().getY();
             this.bipedLeftLeg.rotateAngleZ = 0.017453292F * entityarmorstand.getLeftLegRotation().getZ();
-            this.bipedLeftLeg.setRotationPoint(1.9F, 11.0F, 0.0F);
+            this.bipedLeftLeg.setRotationPoint(1.9F, 11.0F, 0F);
             this.bipedRightLeg.rotateAngleX = 0.017453292F * entityarmorstand.getRightLegRotation().getX();
             this.bipedRightLeg.rotateAngleY = 0.017453292F * entityarmorstand.getRightLegRotation().getY();
             this.bipedRightLeg.rotateAngleZ = 0.017453292F * entityarmorstand.getRightLegRotation().getZ();
-            this.bipedRightLeg.setRotationPoint(-1.9F, 11.0F, 0.0F);
+            this.bipedRightLeg.setRotationPoint(-1.9F, 11.0F, 0F);
             copyModelAngles(this.bipedHead, this.bipedHeadwear);
-        }else{
+        } else {
             super.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entityIn);
         }
+        float f = 0;
+        float f1 = 12;
+        if(isSneak){
+            f = -1;
+            f1 = 10;
+        }
+        this.robeLower.rotateAngleX = Math.min(0, Math.min(this.bipedLeftLeg.rotateAngleX, this.bipedRightLeg.rotateAngleX)) - this.bipedBody.rotateAngleX;
+        this.robeLower.rotationPointZ = f;
+        this.robeLower.rotationPointY = f1;
+
+        this.robeLowerBack.rotateAngleX =  -Math.max(this.bipedLeftLeg.rotateAngleX, this.bipedRightLeg.rotateAngleX);
     }
 }
