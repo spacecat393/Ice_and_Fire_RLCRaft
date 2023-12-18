f2621aa458a55299e9b7f3a77a12ac51dc0325ff
kotlin-programmer <seattlesoftwareengineer@gmail.com>
2023-12-18 01:35:00 -0800

    Introduce new capes & update config

--- src/main/java/com/github/alexthe666/iceandfire/client/render/entity/RenderModCapes.java
+++ src/main/java/com/github/alexthe666/iceandfire/client/render/entity/RenderModCapes.java
@@ -16,19 +16,33 @@ import java.util.UUID;
 
 @SideOnly(Side.CLIENT)
 public class RenderModCapes {
-	private static final ResourceLocation redTex = new ResourceLocation("iceandfire", "textures/models/misc/cape_fire.png");
-	private static final ResourceLocation redElytraTex = new ResourceLocation("iceandfire", "textures/models/misc/elytra_fire.png");
-	private static final ResourceLocation blueTex = new ResourceLocation("iceandfire", "textures/models/misc/cape_ice.png");
-	private static final ResourceLocation blueElytraTex = new ResourceLocation("iceandfire", "textures/models/misc/elytra_ice.png");
-	private static final ResourceLocation betaTex = new ResourceLocation("iceandfire", "textures/models/misc/cape_beta.png");
-	private static final ResourceLocation betaElytraTex = new ResourceLocation("iceandfire", "textures/models/misc/elytra_beta.png");
-	private static final UUID[] redcapes = new UUID[]{
-	/* zeklo */UUID.fromString("59efccaf-902d-45da-928a-5a549b9fd5e0"),
-	/* Alexthe666 */UUID.fromString("71363abe-fd03-49c9-940d-aae8b8209b7c")};
-	private static final UUID[] bluecapes = new UUID[]{
-	/* Raptorfarian */UUID.fromString("0ed918c8-d612-4360-b711-cd415671356f"),
-	/*Zyranna*/		UUID.fromString("5d43896a-06a0-49fb-95c5-38485c63667f")};
-	private static final UUID[] betatesters = new UUID[]{
+	private static final ResourceLocation shivaxiCapeTex = new ResourceLocation("iceandfire", "textures/models/misc/cape_shivaxi.png");
+	private static final ResourceLocation shivaxiElytraTex = new ResourceLocation("iceandfire", "textures/models/misc/elytra_shivaxi.png");
+	private static final ResourceLocation artsyDyCapeTex = new ResourceLocation("iceandfire", "textures/models/misc/cape_artsydy.png");
+	private static final ResourceLocation artsyDyElytraTex = new ResourceLocation("iceandfire", "textures/models/misc/elytra_artsydy.png");
+	private static final ResourceLocation eagleCapeTex = new ResourceLocation("iceandfire", "textures/models/misc/cape_eagle.png");
+	private static final ResourceLocation eagleElytraTex = new ResourceLocation("iceandfire", "textures/models/misc/elytra_eagle.png");
+	private static final ResourceLocation fireCapeTex = new ResourceLocation("iceandfire", "textures/models/misc/cape_fire.png");
+	private static final ResourceLocation fireElytraTex = new ResourceLocation("iceandfire", "textures/models/misc/elytra_fire.png");
+	private static final ResourceLocation iceCapeTex = new ResourceLocation("iceandfire", "textures/models/misc/cape_ice.png");
+	private static final ResourceLocation iceElytraTex = new ResourceLocation("iceandfire", "textures/models/misc/elytra_ice.png");
+	private static final ResourceLocation lightningCapeTex = new ResourceLocation("iceandfire", "textures/models/misc/cape_lightning.png");
+	private static final ResourceLocation lightningElytraTex = new ResourceLocation("iceandfire", "textures/models/misc/elytra_lightning.png");
+
+	private static final UUID[] firecapes = new UUID[] {
+			User.Alexthe666,
+			User.Zeklo
+	};
+	private static final UUID[] iceCapes = new UUID[] {
+			User.Meldexun,
+			User.Raptorfarian,
+			User.Zyranna
+	};
+	private static final UUID[] lightningCapes = new UUID[] {
+			User.Fonnymunkey,
+			User.Kotlin_Dev,
+			User.NLBlackHeavenNL,
+			User.Rayquazafallout
 	};
 
 	private static Field playerInfoField;
@@ -64,28 +78,39 @@ public class RenderModCapes {
 					var5.printStackTrace();
 				}
 
-				if(textureMap != null) {
-					if(this.hasBetaCape(event.getEntityPlayer().getUniqueID())) {
-						textureMap.put(MinecraftProfileTexture.Type.CAPE, betaTex);
-						textureMap.put(MinecraftProfileTexture.Type.ELYTRA, betaElytraTex);
+				if (textureMap != null) {
+					UUID uniqueID = event.getEntityPlayer().getUniqueID();
+					if (User.Shivaxi.equals(uniqueID)) {
+						textureMap.put(MinecraftProfileTexture.Type.CAPE, shivaxiCapeTex);
+						textureMap.put(MinecraftProfileTexture.Type.ELYTRA, shivaxiElytraTex);
 					}
-
-					if(this.hasRedCape(event.getEntityPlayer().getUniqueID())) {
-						textureMap.put(MinecraftProfileTexture.Type.CAPE, redTex);
-						textureMap.put(MinecraftProfileTexture.Type.ELYTRA, redElytraTex);
+					if (User.ArtsyDy.equals(uniqueID)) {
+						textureMap.put(MinecraftProfileTexture.Type.CAPE, artsyDyCapeTex);
+						textureMap.put(MinecraftProfileTexture.Type.ELYTRA, artsyDyElytraTex);
 					}
-
-					if(this.hasBlueCape(event.getEntityPlayer().getUniqueID())) {
-						textureMap.put(MinecraftProfileTexture.Type.CAPE, blueTex);
-						textureMap.put(MinecraftProfileTexture.Type.ELYTRA, blueElytraTex);
+					if (User.NLBlackEagle.equals(uniqueID)) {
+						textureMap.put(MinecraftProfileTexture.Type.CAPE, eagleCapeTex);
+						textureMap.put(MinecraftProfileTexture.Type.ELYTRA, eagleElytraTex);
+					}
+					if (this.hasFireCape(uniqueID)) {
+						textureMap.put(MinecraftProfileTexture.Type.CAPE, fireCapeTex);
+						textureMap.put(MinecraftProfileTexture.Type.ELYTRA, fireElytraTex);
+					}
+					if (this.hasIceCape(uniqueID)) {
+						textureMap.put(MinecraftProfileTexture.Type.CAPE, iceCapeTex);
+						textureMap.put(MinecraftProfileTexture.Type.ELYTRA, iceElytraTex);
+					}
+					if (this.hasLightningCape(uniqueID)) {
+						textureMap.put(MinecraftProfileTexture.Type.CAPE, lightningCapeTex);
+						textureMap.put(MinecraftProfileTexture.Type.ELYTRA, lightningElytraTex);
 					}
 				}
 			}
 		}
 	}
 
-	private boolean hasRedCape(UUID uniqueID) {
-		for (UUID uuid1 : redcapes) {
+	private boolean hasFireCape(UUID uniqueID) {
+		for (UUID uuid1 : firecapes) {
 			if (uniqueID.equals(uuid1)) {
 				return true;
 			}
@@ -93,8 +118,8 @@ public class RenderModCapes {
 		return false;
 	}
 
-	private boolean hasBlueCape(UUID uniqueID) {
-		for (UUID uuid1 : bluecapes) {
+	private boolean hasIceCape(UUID uniqueID) {
+		for (UUID uuid1 : iceCapes) {
 			if (uniqueID.equals(uuid1)) {
 				return true;
 			}
@@ -102,12 +127,27 @@ public class RenderModCapes {
 		return false;
 	}
 
-	private boolean hasBetaCape(UUID uniqueID) {
-		for (UUID uuid1 : betatesters) {
+	private boolean hasLightningCape(UUID uniqueID) {
+		for (UUID uuid1 : lightningCapes) {
 			if (uniqueID.equals(uuid1)) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+	private static class User {
+		private static final UUID Alexthe666 = UUID.fromString("71363abe-fd03-49c9-940d-aae8b8209b7c");
+		private static final UUID ArtsyDy = UUID.fromString("14bba455-affa-46d0-9cf0-806cc0f3d454");
+		private static final UUID Fonnymunkey = UUID.fromString("648ab92b-e045-43c8-af1e-85a069d7bf42");
+		private static final UUID Kotlin_Dev = UUID.fromString("2cda280f-2547-4f0c-88b3-05fa236c4bf4");
+		private static final UUID Meldexun = UUID.fromString("fe29a2f2-aefc-4ad5-a2b9-86ee8bc41a66");
+		private static final UUID NLBlackEagle = UUID.fromString("82da4f98-9835-4744-bcd5-62ba2d5e2df6");
+		private static final UUID NLBlackHeavenNL = UUID.fromString("ba54bb01-4afa-4543-a877-9642e4c582c9");
+		private static final UUID Raptorfarian = UUID.fromString("0ed918c8-d612-4360-b711-cd415671356f");
+		private static final UUID Rayquazafallout = UUID.fromString("369a3e3f-371c-4ce6-ba44-48c3eb91ce8b");
+		private static final UUID Shivaxi = UUID.fromString("cdfccefb-1a2e-4fb8-a3b5-041da27fde61");
+		private static final UUID Zeklo = UUID.fromString("59efccaf-902d-45da-928a-5a549b9fd5e0");
+		private static final UUID Zyranna = UUID.fromString("5d43896a-06a0-49fb-95c5-38485c63667f");
 	}
 }
